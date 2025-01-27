7f81eef51f806180381bc76141015119c120ae43
thoriqdharmawan <thoriqd21@gmail.com>
2025-01-27 22:05:21 +0700

    chore: add tailwind prettier

--- components/theme/valency/WeddingDate.tsx
+++ components/theme/valency/WeddingDate.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function WeddingDate({ data }: { data: Wedding }) {
   return (
-    <div className="flex flex-col gap-4 px-6 py-12 text-center items-center">
-      <div className="rounded-3xl flex flex-col items-center gap-3 border-2 border-primary p-4">
+    <div className="flex flex-col gap-4 px-12 py-12 text-center items-center">
+      <div className="rounded-t-full flex flex-col items-center gap-3 border-2 border-primary p-4">
         <Image
           src="https://picsum.photos/300/400"
           height={300}
           width={200}
           alt="date"
-          className="rounded-md"
+          className="rounded-3xl"
         />
 
         <h1 className="text-3xl font-semibold">Our Journey</h1>
