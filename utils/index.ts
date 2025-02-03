ed3fb60cdca82964aae3a0f0f37a4eade6a62f80
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-03 12:23:29 +0700

    chore: implement formatter

--- utils/index.ts
+++ utils/index.ts
@@ -1,33 +1,31 @@
 export const getDay = (dateString: string): string => {
-  const date = new Date(dateString);
-  return Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-};
+  const date = new Date(dateString)
+  return Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date)
+}
 
 export const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
+  const date = new Date(dateString)
 
-  const month = new Intl.DateTimeFormat("en-US", { month: "long" })
-    .format(date)
-    .toUpperCase();
+  const month = new Intl.DateTimeFormat("en-US", { month: "long" }).format(date).toUpperCase()
 
-  const day = date.getDate();
+  const day = date.getDate()
 
-  const year = date.getFullYear();
+  const year = date.getFullYear()
 
   const getDaySuffix = (day: number): string => {
-    if (day >= 11 && day <= 13) return "th";
-    const lastDigit = day % 10;
+    if (day >= 11 && day <= 13) return "th"
+    const lastDigit = day % 10
     switch (lastDigit) {
       case 1:
-        return "st";
+        return "st"
       case 2:
-        return "nd";
+        return "nd"
       case 3:
-        return "rd";
+        return "rd"
       default:
-        return "th";
+        return "th"
     }
-  };
+  }
 
-  return `${month}, ${day}<sup>${getDaySuffix(day)}</sup> ${year}`;
-};
+  return `${month}, ${day}<sup>${getDaySuffix(day)}</sup> ${year}`
+}
