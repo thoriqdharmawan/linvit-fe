46367d5fd5bd57aec99e2c3e208c7a39b97fc90c
thoriqdharmawan <thoriqd21@gmail.com>
2025-04-29 22:18:37 +0700

    refactor: improve formatting and structure of VideoOpening component

--- components/theme/valency/sections/VideoOpening.tsx
+++ components/theme/valency/sections/VideoOpening.tsx
@@ -1,32 +1,24 @@
-import { Wedding } from "@/interfaces";
+import { Wedding } from "@/interfaces"
 
 export default function VideoOpening({ data }: { data: Wedding }) {
   return (
-    <div className="h-full w-full flex relative justify-center items-center">
-      <video autoPlay muted loop className="object-cover h-full w-full">
+    <div className="relative flex h-full w-full items-center justify-center">
+      <video autoPlay muted loop className="h-full w-full object-cover">
         <source src="/opening.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <div className="absolute opacity-0 animate-fadeIn text-center px-20">
-        <p className="castoro-regular mb-8 text-body">THE WEDDING OF</p>
+      <div className="absolute animate-fadeIn px-20 text-center opacity-0">
+        <p className="castoro-regular mb-4 text-gray-500">THE WEDDING OF</p>
 
-        <div className="gap-3 flex flex-col">
-          <h2 className="text-4xl font-semibold castoro-regular text-primary">
-            {data.groom_nickname}
-          </h2>
-          <p className="text-4xl font-semibold pinyon-script-regular text-body">
-            and
-          </p>
-          <h2 className="text-4xl font-semibold castoro-regular text-primary">
-            {data.bride_nickname}
-          </h2>
+        <div className="flex flex-col gap-3">
+          <h2 className="castoro-regular text-4xl font-semibold text-primary">{data.groom_nickname} Dharmawan</h2>
+          <p className="pinyon-script-regular text-8xl font-semibold text-gray-500">&</p>
+          <h2 className="castoro-regular text-4xl font-semibold text-primary">{data.bride_nickname}tun Hasanah</h2>
         </div>
 
-        <p className="castoro-regular text-base mt-8 text-body">
-          {data.wedding_date}
-        </p>
+        <p className="castoro-regular mt-8 text-base text-gray-500">{data.wedding_date}</p>
       </div>
     </div>
-  );
+  )
 }
