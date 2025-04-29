ab85643eee189937f2d13e72ac2dba6a68237816
thoriqdharmawan <thoriqd21@gmail.com>
2025-04-29 23:00:26 +0700

    fix: add object-contain class to Image component in PhotoHightlight for better styling

--- components/theme/valency/sections/PhotoHightlight.tsx
+++ components/theme/valency/sections/PhotoHightlight.tsx
@@ -4,7 +4,7 @@ import type { Wedding } from "@/interfaces"
 export default function PhotoHightlight({ data }: { data: Wedding }) {
   return (
     <div data-sal="slide-up" data-sal-duration="2000" className="relative h-[645px] w-full">
-      <Image src={data.photo_highlight_one} fill alt="hightlight" />
+      <Image src={data.photo_highlight_one} fill alt="hightlight" className="object-contain" />
     </div>
   )
 }
