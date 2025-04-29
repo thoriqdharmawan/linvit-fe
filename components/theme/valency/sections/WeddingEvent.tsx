7393fe5ddaad6d179e182a23f889c566e6d4661f
thoriqdharmawan <thoriqd21@gmail.com>
2025-04-29 21:09:19 +0700

    feat: add LocationIcon component and replace SVG in WeddingEvent section

--- components/theme/valency/sections/WeddingEvent.tsx
+++ components/theme/valency/sections/WeddingEvent.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import type { Wedding } from "@/interfaces"
 import { formatDate, getDay } from "@/utils"
+import LocationIcon from "@/components/icons/LocationIcon"
 
 interface Props {
   title: string
@@ -54,9 +55,8 @@ const Card = (props: Props) => {
             rel="noopener noreferrer"
             className="castoro-regular flex items-center justify-center gap-2 rounded-full border-0 bg-gradient-to-r from-secondary to-primary p-3 text-sm text-white"
           >
-            <svg className="size-5 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-              <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
-            </svg>
+            <LocationIcon />
+
             <p className="text-sm leading-3">VIEW LOCATION</p>
           </a>
         </div>
